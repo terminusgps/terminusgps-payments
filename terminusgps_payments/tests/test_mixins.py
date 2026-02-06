45ef901c1b5046b156075449932377c501bda45e
darthnall <blake@proton.me>
2026-02-06 12:44:14 -0600

    Improved mixin tests

--- terminusgps_payments/tests/test_mixins.py
+++ terminusgps_payments/tests/test_mixins.py
@@ -4,49 +4,44 @@ from django.views.generic import TemplateView
 from terminusgps_payments.mixins import HtmxTemplateResponseMixin
 
 
-class HtmxTemplateResponseMixinTestView(
-    HtmxTemplateResponseMixin, TemplateView
-):
+class TestTemplateView(HtmxTemplateResponseMixin, TemplateView):
     template_name = "test.html"
 
 
 class HtmxTemplateResponseMixinTestCase(TestCase):
     def setUp(self):
-        self.view_cls = HtmxTemplateResponseMixinTestView
+        self.view = TestTemplateView(template_name="test.html")
 
     def test_render_to_response_explicit_partial_name(self):
         """Fails if :py:attr:`template_name` wasn't set to an explicitly provided :py:attr:`partial_name`."""
         headers = {"HX-Request": "true"}
         request = RequestFactory().get("/", headers=headers)
         explicit_partial_name = "test.html#mypartial"
-        view = self.view_cls(partial_name=explicit_partial_name)
-        view.setup(request)
-        view.render_to_response(context=view.get_context_data())
-        self.assertEqual(explicit_partial_name, view.template_name)
+        self.view.partial_name = explicit_partial_name
+        self.view.setup(request)
+        self.view.render_to_response(context=self.view.get_context_data())
+        self.assertEqual(explicit_partial_name, self.view.template_name)
 
     def test_render_to_response_htmx_request(self):
         """Fails if :py:attr:`template_name` wasn't updated on an htmx request."""
         headers = {"HX-Request": "true"}
         request = RequestFactory().get("/", headers=headers)
-        view = self.view_cls()
-        view.setup(request)
-        view.render_to_response(context=view.get_context_data())
-        self.assertEqual("test.html#main", view.template_name)
+        self.view.setup(request)
+        self.view.render_to_response(context=self.view.get_context_data())
+        self.assertEqual("test.html#main", self.view.template_name)
 
     def test_render_to_response_htmx_request_boosted(self):
         """Fails if :py:attr:`template_name` was updated on a boosted htmx request."""
         headers = {"HX-Request": "true", "HX-Boosted": "true"}
         request = RequestFactory().get("/", headers=headers)
-        view = self.view_cls()
-        view.setup(request)
-        view.render_to_response(context=view.get_context_data())
-        self.assertEqual("test.html", view.template_name)
+        self.view.setup(request)
+        self.view.render_to_response(context=self.view.get_context_data())
+        self.assertEqual("test.html", self.view.template_name)
 
     def test_render_to_response_non_htmx_request(self):
         """Fails if :py:attr:`template_name` was updated on a non-htmx request."""
         headers = {}
         request = RequestFactory().get("/", headers=headers)
-        view = self.view_cls()
-        view.setup(request)
-        view.render_to_response(context=view.get_context_data())
-        self.assertEqual("test.html", view.template_name)
+        self.view.setup(request)
+        self.view.render_to_response(context=self.view.get_context_data())
+        self.assertEqual("test.html", self.view.template_name)
