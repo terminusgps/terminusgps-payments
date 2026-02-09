50aca3e0a379fc72a479dc6f0063be42b6cf966d
darthnall <blake@proton.me>
2026-02-09 16:23:05 -0600

    Updated tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,12 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import RequestFactory, TestCase, override_settings
 
 from terminusgps_payments.forms import CustomerPaymentProfileCreateForm
-from terminusgps_payments.models import (
-    CustomerAddressProfile,
-    CustomerPaymentProfile,
-)
+from terminusgps_payments.models import CustomerPaymentProfile
 from terminusgps_payments.views import (
-    CustomerAddressProfileDeleteView,
     CustomerPaymentProfileCreateView,
     CustomerPaymentProfileDeleteView,
     CustomerPaymentProfileDetailView,
@@ -112,7 +108,7 @@ class CustomerAddressProfileDetailViewTestCase(TestCase):
         response = self.client.trace("/address-profiles/1/detail/")
         self.assertEqual(response.status_code, 405)
 
-    def test_get_authenticated_other_user(self):
+    def test_get_authenticated_other(self):
         """Fails if a GET request from another authenticated client returns a response code other than 404."""
         self.client.login(
             **{
@@ -151,6 +147,32 @@ class CustomerAddressProfileListViewTestCase(TestCase):
         """Fails if a GET request from an authenticated client returns a response code other than 200."""
         response = self.client.get("/address-profiles/list/")
         self.assertEqual(response.status_code, 200)
+        self.assertInHTML(
+            '<a class="hover:underline" href="/address-profiles/1/detail/"><li>TestAddress</li></a>',
+            response.rendered_content,
+        )
+        self.assertNotInHTML(
+            '<a class="hover:underline" href="/address-profiles/2/detail/"><li>TestAddress</li></a>',
+            response.rendered_content,
+        )
+
+    def test_get_authenticated_other(self):
+        self.client.login(
+            **{
+                "username": "testuseralt",
+                "password": "super_secure_password1!",
+            }
+        )
+        response = self.client.get("/address-profiles/list/")
+        self.assertInHTML(
+            '<a class="hover:underline" href="/address-profiles/2/detail/"><li>TestAddress</li></a>',
+            response.rendered_content,
+        )
+        self.assertNotInHTML(
+            '<a class="hover:underline" href="/address-profiles/1/detail/"><li>TestAddress</li></a>',
+            response.rendered_content,
+        )
+        self.client.logout()
 
     def test_not_allowed_http_methods(self):
         """Fails if a non-GET request returns a response code other than 405."""
@@ -179,45 +201,61 @@ class CustomerAddressProfileDeleteViewTestCase(TestCase):
     ]
 
     def setUp(self):
-        self.factory = RequestFactory()
-        self.view = CustomerAddressProfileDeleteView()
-        self.user = get_user_model().objects.get(pk=1)
-
-    def test_content_type(self):
-        request = self.factory.get("/address-profiles/1/delete/")
-        request.user = self.user
-        self.view.setup(request)
-        self.assertEqual("text/html", self.view.content_type)
-
-    def test_http_method_names(self):
-        request = self.factory.get("/address-profiles/1/delete/")
-        request.user = self.user
-        self.view.setup(request)
-        self.assertIn("get", self.view.http_method_names)
-        self.assertIn("post", self.view.http_method_names)
-
-    def test_get_queryset_authenticated_user(self):
-        """Fails if :py:meth:`get_queryset` doesn't return all objects associated with the user."""
-        request = self.factory.get("/address-profiles/1/delete/")
-        request.user = self.user
-        self.view.setup(request)
-        self.assertQuerySetEqual(
-            self.view.get_queryset(),
-            CustomerAddressProfile.objects.filter(
-                customer_profile__user=self.user
-            ),
-            ordered=False,
+        self.client.login(
+            **{"username": "testuser", "password": "super_secure_password1!"}
         )
 
-    def test_get_queryset_anonymous_user(self):
-        """Fails if :py:meth:`get_queryset` returns anything other than an empty queryset for an anonymous user."""
-        request = self.factory.get("/address-profiles/1/delete/")
-        self.view.setup(request)
-        self.assertQuerySetEqual(
-            self.view.get_queryset(),
-            CustomerAddressProfile.objects.none(),
-            ordered=False,
+    def tearDown(self):
+        self.client.logout()
+
+    def test_get_anonymous(self):
+        """Fails if a GET request from an anonymous client returns a response code other than 302."""
+        self.client.logout()
+        response = self.client.get("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_authenticated(self):
+        """Fails if a GET request from an authenticated client returns a response code other than 200."""
+        response = self.client.get("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_authenticated_other(self):
+        """Fails if a GET request from another authenticated client returns a response code other than 404."""
+        self.client.login(
+            **{
+                "username": "testuseralt",
+                "password": "super_secure_password1!",
+            }
         )
+        response = self.client.get("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 404)
+        self.client.logout()
+
+    def test_post_anonymous(self):
+        """Fails if a POST request from an anonymous client returns a response code other than 302."""
+        self.client.logout()
+        response = self.client.post("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_authenticated(self):
+        """Fails if a POST request from an authenticated client returns a response code other than 200."""
+        response = self.client.post("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_allowed_http_methods(self):
+        """Fails if a non-GET request returns a response code other than 405."""
+        response = self.client.put("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
+        response = self.client.patch("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
+        response = self.client.delete("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
+        response = self.client.head("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
+        response = self.client.options("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
+        response = self.client.trace("/address-profiles/1/delete/")
+        self.assertEqual(response.status_code, 405)
 
 
 @override_settings(ROOT_URLCONF="src.urls")
